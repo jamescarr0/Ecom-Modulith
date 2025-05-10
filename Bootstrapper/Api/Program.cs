1a4e9a225986f70ecffe25130743806ed7506476
jamescarr0 <jamescarr0@hotmail.com>
2025-05-10 20:25:07 +0100

    Wire up dependencies and register services complete

--- Bootstrapper/Api/Program.cs
+++ Bootstrapper/Api/Program.cs
@@ -1,9 +1,20 @@
+using Basket;
+using Catalog;
+using Ordering;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+builder.Services
+    .AddBasketModule(builder.Configuration)
+    .AddCatalogModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP Pipeline
+app.UseBasketModule()
+    .UseCatalogModule()
+    .UseOrderingModule();
 
 app.Run();
