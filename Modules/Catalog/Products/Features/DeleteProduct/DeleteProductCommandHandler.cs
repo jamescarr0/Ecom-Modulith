68f797085295f737c84f8f7cac6250d37f15f748
jamescarr0 <jamescarr0@hotmail.com>
2025-05-15 11:17:53 +0100

    Delete Product Handler Complete

---
+++ Modules/Catalog/Products/Features/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Data;
+using Shared.CQRS;
+
+namespace Catalog.Products.Features.DeleteProduct;
+
+internal record DeleteProductCommand(Guid ProductId) : ICommand<DeleteProductResult>;
+internal record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHandler(CatalogDbContext dbContext)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = await dbContext.Products.FindAsync([command.ProductId], cancellationToken: cancellationToken);
+
+        if (product is null)
+        {
+            throw new Exception($"Product not found with Id: {command.ProductId}");
+        }
+
+        dbContext.Remove(product);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+
+    }
+}
