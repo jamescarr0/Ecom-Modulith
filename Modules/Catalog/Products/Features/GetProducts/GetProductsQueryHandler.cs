af71a76f0d6175bf9a937529d31deb310601c5e3
jamescarr0 <jamescarr0@hotmail.com>
2025-05-15 12:20:01 +0100

    Get Products Query Handler complete

---
+++ Modules/Catalog/Products/Features/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Data;
+using Catalog.Products.Dtos;
+using Catalog.Products.Models;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using Shared.CQRS;
+
+namespace Catalog.Products.Features.GetProducts;
+
+internal record GetProductsQuery() : IQuery<GetProductsResult>;
+internal record GetProductsResult(IEnumerable<ProductDto> ProductsDto);
+
+internal class GetProductsQueryHandler(CatalogDbContext dbContext) : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        var products = await dbContext.Products
+            .AsNoTracking()
+            .OrderBy(p => p.Name)
+            .ToListAsync(cancellationToken);
+
+        var productsDtos = products.Adapt<List<ProductDto>>();
+
+        return new GetProductsResult(productsDtos);
+    }
+}
